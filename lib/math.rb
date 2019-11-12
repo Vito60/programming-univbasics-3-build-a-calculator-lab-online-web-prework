244e8a9e0c2d225d67e9628ce6e2d90d7ccdb690
Randall D Williams <randallw1996@gmail.com>
2019-11-12 01:14:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,14 +4,17 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  subtraction = num2 - num2
 
 end
 
 def division(num1, num2)
+  division = num1 / num2
 
 end
 
 def multiplication(num1, num2)
+  multiplication = num1 * num2
 
 end
 
